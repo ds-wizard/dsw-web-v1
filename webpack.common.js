3c2961a59f4b747e3dc4ba76694f694da5546717
Jan Slifka <jan.slifka@gmail.com>
2018-08-24 13:36:48 +0200

    Add enterprise page

--- webpack.common.js
+++ webpack.common.js
@@ -4,9 +4,10 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const pages = [
+    'contact',
+    'enterprise',
     'index',
-    'privacy',
-    'contact'
+    'privacy'
 ]
 
 module.exports = {
