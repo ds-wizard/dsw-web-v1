0a76f5f3d3fef98447fc3bf23ad0b707a75939da
Jan Slifka <jan.slifka@gmail.com>
2019-09-15 16:46:22 +0200

    New web

--- src/main.js
+++ src/main.js
@@ -5,3 +5,18 @@ if (process.env.NODE_ENV !== 'production') {
     require('./media.pug')
     require('./sass/main.sass')
 }
+
+
+window.onload = function() {
+    var mobileMenu = document.querySelector('.mobile-menu')
+    var openMenu = document.querySelector('.open-mobile-menu')
+    var closeMenu = document.querySelector('.close-mobile-menu')
+
+    openMenu.onclick = function() {
+        mobileMenu.classList.add('active')
+    }
+
+    closeMenu.onclick = function() {
+        mobileMenu.classList.remove('active')
+    }
+}
